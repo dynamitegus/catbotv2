49f313f440851bd2e6ec19f8c9309c26bc3cd974
dynamitegus <dynamitegus@dynamitegus.org>
2025-04-29 11:21:14 +1000

    counter commands

--- src/commands/counter.ts
+++ src/commands/counter.ts
@@ -7,16 +7,37 @@ import {
   SimpleCommandOptionType,
   Slash,
 } from "discordx";
-
+var x = 0;
 @Discord()
 export class Example {
+  // make single handler for simple and slash command
+
+  async count(command: CommandInteraction | Message): Promise<void> {
+    await command.reply("I like it, Thanks" + x.toString());
+  }
+
   @SimpleCommand({ name: "count" })
-  async count(command: SimpleCommandMessage): Promise<void> {
-    const member = command.message.member;
-    if (member) {
-      await command.message.reply(`👋 ${member.toString()}`);
-    } else {
-      await command.message.reply("👋 hello");
-    }
+  async simpleCount(command: SimpleCommandMessage): Promise<void> {
+    await this.count(command.message);
+  }
+
+  @Slash({ description: "counter", name: "count" })
+  async slashCount(command: CommandInteraction): Promise<void> {
+    await this.count(command);
+  }
+
+  async countadd(command: CommandInteraction | Message): Promise<void> {
+    x += 1;
+    await command.reply("current count: " + x.toString());
+  }
+
+  @SimpleCommand({ name: "countadd", aliases: ["count add"] })
+  async simpleCountadd(command: SimpleCommandMessage): Promise<void> {
+    await this.countadd(command.message);
+  }
+
+  @Slash({ description: "add to counter", name: "countadd"})
+  async slashCountadd(command: CommandInteraction): Promise<void> {
+    await this.countadd(command);
   }
 }
